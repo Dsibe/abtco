d3868da59579a484f953e86e37e91711f3f43198
Dsibe <darik.pc@gmail.com>
2019-07-20 18:04:12 +0300

    'views.py'

--- main/views.py
+++ main/views.py
@@ -12,17 +12,17 @@ from users.models import *
 
 
 def departments(request):
-    return render(request, r'main\departments.html')
+    return render(request, r'main/departments.html')
 
 def main(request):
-    return render(request, r'main\main.html')
+    return render(request, r'main/main.html')
 
 
 def developing(request):
-    return render(request, r'main\developing.html')
+    return render(request, r'main/developing.html')
 
 def junior_studio(request):
-    return render(request, r'main\junior_studio.html')
+    return render(request, r'main/junior_studio.html')
 
 def contact_us(request):
     if request.method == 'GET':
@@ -35,62 +35,62 @@ def contact_us(request):
             message = form.cleaned_data['message']
             send_mail(f'New contact-us message from {email}, name: {name}', message, 'school@abtco.us', ['school@abtco.us', 'darik.pc@gmail.com', 'abt.company@aol.com'])
             return HttpResponse('You message has been sended succesfully')
-    return render(request, r'main\contact_us.html', context={'form': form})
+    return render(request, r'main/contact_us.html', context={'form': form})
 
 def trading(request):
-    return render(request, r'main\trading.html')
+    return render(request, r'main/trading.html')
 
 def websites_developing(request):
-    return render(request, r'main\websites_developing.html')
+    return render(request, r'main/websites_developing.html')
 
 def fuel_additives(request):
-    return render(request, r'main\fuel_additives.html')
+    return render(request, r'main/fuel_additives.html')
 
 def trading_es(request):
-    return render(request, r'main\trading_es.html')
+    return render(request, r'main/trading_es.html')
 
 def trading_fr(request):
-    return render(request, r'main\trading_fr.html')
+    return render(request, r'main/trading_fr.html')
 
 def trading_en(request):
-    return render(request, r'main\trading_en.html')
+    return render(request, r'main/trading_en.html')
 
 def trading_ru(request):
-    return render(request, r'main\trading_ru.html')
+    return render(request, r'main/trading_ru.html')
 
 def trading_ua(request):
-    return render(request, r'main\trading_ua.html')
+    return render(request, r'main/trading_ua.html')
 
 
 
 def trading_es_i(request):
-    return render(request, r'main\trading_es_i.html')
+    return render(request, r'main/trading_es_i.html')
 
 def trading_fr_i(request):
-    return render(request, r'main\trading_fr_i.html')
+    return render(request, r'main/trading_fr_i.html')
 
 def trading_en_i(request):
-    return render(request, r'main\trading_en_i.html')
+    return render(request, r'main/trading_en_i.html')
 
 def trading_ru_i(request):
-    return render(request, r'main\trading_ru_i.html')
+    return render(request, r'main/trading_ru_i.html')
 
 def trading_ua_i(request):
-    return render(request, r'main\trading_ua_i.html')
+    return render(request, r'main/trading_ua_i.html')
 
 
 
 def trading_es_g(request):
-    return render(request, r'main\trading_es_g.html')
+    return render(request, r'main/trading_es_g.html')
 
 def trading_fr_g(request):
-    return render(request, r'main\trading_fr_g.html')
+    return render(request, r'main/trading_fr_g.html')
 
 def trading_en_g(request):
-    return render(request, r'main\trading_en_g.html')
+    return render(request, r'main/trading_en_g.html')
 
 def trading_ru_g(request):
-    return render(request, r'main\trading_ru_g.html')
+    return render(request, r'main/trading_ru_g.html')
 
 def trading_ua_g(request):
-    return render(request, r'main\trading_ua_g.html')
+    return render(request, r'main/trading_ua_g.html')
