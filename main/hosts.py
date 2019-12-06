54f20cab4400a2baa43d0bafb0f97b3d9f78e93d
Dsibe <darik.pc@gmail.com>
2019-12-06 12:28:39 +0200

    'views.py'

--- main/hosts.py
+++ main/hosts.py
@@ -3,8 +3,7 @@ from django_hosts import patterns, host
 
 host_patterns = patterns(
     '',
-    host(r'khatynka', 'khatynka.urls', name='khatynka'),
-    host(r'www.khatynka', 'khatynka.urls', name='khatynka'),
     host(r'www', 'main.urls', name='www'),
     host(r'', 'main.urls', name='root'),
+    host(r'khatynka', 'khatynka.urls', name='khatynka'),
 )
