035c59a0c4bff44693451d78bfb6a1a9be5eca3d
Dsibe <darik.pc@gmail.com>
2020-05-30 00:06:06 +0300

    'views.py'

--- abtco/settings.py
+++ abtco/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.environ.get(
     'DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'www.abtco.us',
