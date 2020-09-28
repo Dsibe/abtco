dcedd2a98159c924aeca33e083cfeaee05484d94
unknown <darik.pc@gmail.com>
2020-09-28 22:16:44 +0300

    Se3

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: gunicorn abtco.wsgi --log-file -
---check-static /app/static --check-static /static
--- abtco/settings.py
+++ abtco/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django_hosts.middleware.HostsRequestMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
